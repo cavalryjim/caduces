94b95f3f8d0d4a33ebee7e13905270c561de703f
James Davis <james.davisphd@gmail.com>
2011-05-20 09:09:34 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Caduces::Application.config.secret_token = '50e53994c4901b4366912762cb2950a3b9a96713743751154b84439dd59eeb87465f5944832832377d3f30c80ec88c672336ee04510c6df7a71d8911dcee55e0'
